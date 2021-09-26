6bdfd04ae829af459782c2682f0950d172e11136
Tahir <muhammad.tahir@afiniti.com>
2021-09-27 00:07:29 +0500

    updated sorting file

---
+++ Sorting.clj
@@ -0,0 +1,35 @@
+Sorting
+    merge sort (nlogn)
+        O(n)
+    quick sort (nlogn) (n^2) 
+        S(log(n))
+        
+Searching
+    binary search (logn)
+    linear search (n)
+Arrays
+    set -> (n)
+    remove -> (n)
+    get -> (1)
+    search -> (n)
+        S(n)
+linked list
+    search -> (n)
+    access -> (n)
+    remove -> (1)
+    set -> (1)
+stacks
+    push -> (1)
+    pop -> (1)
+    peek -> (1)
+    search -> (n)
+queues
+    enqueue -> (1)
+    dequeue -> (1)
+
+graphs
+trees
+hashmaps
+    set -> (1)
+    get -> (1)
+    collision -> (n)
